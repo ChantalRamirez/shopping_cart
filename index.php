05827dd5a3afe53928dd83f6d4d044fd39bd5f85
ChantalRamirez <ramirez_yessica@ucol.mx>
2023-10-24 09:33:59 -0600

    Add button cartShopping

--- index.php
+++ index.php
@@ -7,17 +7,23 @@
 <section class="container" id="product_list" >
   <div class="floatright position-relative" >
       <div class="mb-4" >
-        <span class="font-weight-bold bg-white" style="margin-right:10px;">{{currency(cartTotal)}}</span>
-        <button @click="displayCart = !displayCart" class="btn btn-success btn-sm ml-3" id="cartDropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="font-weight-bold bg-white" style="margin-right:10px;" :class="totalColor"><curr :amt = "cartTotal"></curr></span>
+        <button :class="cartBtn" @click="displayCart = !displayCart" class="btn btn-sm ml-3" id="cartDropdown" aria-haspopup="true" aria-expanded="false">
           <i class="glyphicon glyphicon-shopping-cart mr-4" style="margin-right:5px;"></i>
           {{cart.length}}
         </button>
       </div>
-      <div v-if="displayCart" class="list-group position-absolute" aria-labelledby="cartDropdown" style="top: 30px;right: 0; width:300px; z-index:100;">
-        <div v-for="(item, index) in cart" :key="index" class="list-group-item " style="display:flex;">
-          <div style="margin-right: 10px;">{{item.name}} </div>
-          <div class="ml-3 font-weight-bold">{{currency(item.price)}}</div>
-        </div>
+      <div class="dropdown-clip" >
+        <transition name='dropdown'
+          @enter = 'transitionColor'
+          @after-leave ='resetColor' >
+          <div v-if="displayCart" class="list-group " aria-labelledby="cartDropdown" style="position:absolute; top: 30px;right: 0; width:300px; z-index:100;">
+            <div v-for="(item, index) in cart" :key="index" class="list-group-item " style="display:flex;">
+              <div style="margin-right: 10px;">{{item.name}} </div>
+              <div class="ml-3 font-weight-bold"><curr :amt = "item.price"></curr></div>
+            </div>
+          </div>
+        </transition>
       </div>
   </div>
 
@@ -26,29 +32,17 @@
   <input v-model.number="max" class="form-control" id="max-price">
   <input v-model.number="max" type="range" class="form-range" min="0" max="130">
 
-  <div class="showLabels">
+  <!-- <div class="showLabels">
     <input v-model="displayLabels" type="checkbox" id="showLabels" >
     <span class="span-showLabels" >Show labels</span>
-  </div>
+  </div> -->
  
-  <div v-for="(item, index) in filteredProducts" :key="item.id" id="display-list" class="row mb-3 display-center mt-3">
-      <div class="col-sm-1 m-auto">
-        <button @click="addToCart(item)" class="btn btn-success">+</button>
-      </div>
-      <div class="col-sm-2">
-        <img class="image_product" :src="item.image" :alt="item.name">
-      </div>
-      <div class="col-sm-9">
-        <h3 class="text-info">{{index +1}}) {{item.name}}</h3>
-        <p class="mb-0">{{item.description}}</p>
-        <div class="floatright">
-          <span class="label label-success" v-if="item.price>=90 && displayLabels ">premier</span>
-          <span class="label label-primary" v-else-if="(item.price<90 && item.price>10) && displayLabels">value</span>
-          <span class="label label-danger" v-else-if="item.price<10 && displayLabels" >sale</span>
-          {{currency(item.price)}}</div>
-      </div>
-  
-  </div>
+  <custom-alert type="danger" close="true" v-if="cartTotal >= 100" ></custom-alert>
+  <transition-group name="products" appear>
+    <div v-for="(item, index) in filteredProducts" :key="item.id" id="display-list" class="row mb-3 display-center mt-3">
+        <product :item="item" :index="index" @add-to-cart="addToCart"></product>
+    </div>
+  </transition-group>
 </section>
 <script src="vue.js"></script>
 
--- vue.js
+++ vue.js
@@ -1,39 +1,94 @@
-const { createApp } = Vue
-  createApp({
-    data(){
-      return{
-        displayLabels: true,
-        max: 50,
-        cart:[],
-        displayCart: false,        
-        mensaje: 'Hola pondremos aquí una lista',
-        products: [],
-      }
-    },
-    created(){
-      fetch("https://hplussport.com/api/products/order/price")
-      .then(response => response.json())
-      .then(data =>{
-        this.products = data;
-      })
-    },
-    computed:{
-      filteredProducts(){
-        return this.products.filter(item => (item.price < this.max));
-      },
-      cartTotal(){
-        return this.cart.reduce((inc, item) => Number(item.price) + inc, 0)
-      }
-    },
-    methods:{
-      addToCart(product){
-        this.cart.push(product)
-      },
-      currency(value){
-        return `$ ${Number.parseFloat(value).toFixed(2)}`
-      }
+const App = Vue.createApp({
+  data(){
+    return{
+      max: 50,
+      cart:[],
+      displayCart: false,        
+      mensaje: 'Hola pondremos aquí una lista',
+      products: [],
     }
-  }).mount('#product_list')
+  },
+  created(){
+    fetch("https://hplussport.com/api/products/order/price")
+    .then(response => response.json())
+    .then(data =>{
+      this.products = data;
+    })
+  },
+  computed:{
+    cartBtn(){
+      return{
+        'btn-secondary': this.cartTotal <= 100,
+        'btn-success': this.cartTotal > 100,
+        'btn-danger': this.cartTotal > 200,
+      }
+    },
+    filteredProducts(){
+      return this.products.filter(item => (item.price < this.max));
+    },
+    cartTotal(){
+      return this.cart.reduce((inc, item) => Number(item.price) + inc, 0)
+    }
+  },
+  methods:{
+    transitionColor(el){
+      this.totalColor = 'text-danger';
+    },
+    resetColor(){
+      this.totalColor = 'text-secondary';
+    },
+    addToCart(product){
+      this.cart.push(product)
+      if (this.cartTotal >= 100) {this.salesBtn = 'btn-success'}
+    },
+    // currency(value){
+    //   return `$ ${Number.parseFloat(value).toFixed(2)}`
+    // }
+  }
+})
+
+App.component('custom-alert', {
+  props: ['type','close'],
+  template: `
+  <div :class="[(type ? 'alert-' + type: 'alert-info'), (close ? 'alert-dismissible' : '')]" class="alert" role="alert" >
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">X</span></button>
+  <slot> Congrats on getting some items in your cart.</slot>  
+  </div>
+  `
+})
+
+App.component('product',{
+  props:['item', 'index'],
+  emits: ['addToCart'],
+  template: `
+      <div class="col-sm-1 m-auto">
+        <button @click="$emit('addToCart', item)" class="btn btn-success">+</button>
+      </div>
+      <div class="col-sm-2">
+        <img class="image_product" :src="item.image" :alt="item.name">
+      </div>
+      <div class="col-sm-9">
+        <h3 class="text-info">{{index +1}}) {{item.name}}</h3>
+        <p class="mb-0">{{item.description}}</p>
+        <div class="floatright">
+          <span class="label label-success" v-if="item.price>=90 && displayLabels ">premier</span>
+          <span class="label label-primary" v-else-if="(item.price<90 && item.price>10) && displayLabels">value</span>
+          <span class="label label-danger" v-else-if="item.price<10 && displayLabels" >sale</span>
+          <curr :amt = "item.price"></curr></div>
+      </div>`
+})
+
+App.component('curr',{
+  props:['amt'],
+  template: `{{dollar(amt)}}`,
+  methods: {
+    dollar(value){
+      return '$' + Number.parseFloat(value).toFixed(2);
+    }
+  }
+})
+
+App.mount('#product_list');
 
  
 
